a72066b19a661ae87f8f4bcc6b542af7641f7b89
Ahmed Essam <ahmedeiisso@gmail.com>
2023-12-02 20:34:19 +0200

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "C. HyperText Markup Language",
+        options: [
+            "A. HighText Machine Language",
+            "B. HighText Machine",
+            "C. HyperText Markup Language",
+            "D. None of these"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which type of JavaScript language is ?",
+        answer: "B. Object-Based",
+        options: [
+            "A. Object-Oriented",
+            "B. Object-Based",
+            "C. Assembly-language",
+            "D. High-level"
+
+        ]
+    },
+    {
+        numb: 3,
+        question: "The function and var are known as ?",
+        answer: "C. Declaration statements",
+        options: [
+            "A. Keywords",
+            "B. Data types",
+            "C. Declaration statements",
+            "D. Prototypes"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which one of the following is the correct way for calling the JavaScript code?",
+        answer: "D. Function/Method",
+        options: [
+            "A. Preprocessor",
+            "B. Triggering Event",
+            "C. RMI",
+            "D. Function/Method"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which of the following type of a variable is volatile?",
+        answer: "A. Mutable variable",
+        options: [
+            "A. Mutable variable",
+            "B. Dynamic variable",
+            "C. Volatile variable",
+            "D. Immutable variable"
+        ]
+    }
+];
